1ca07df10912e299a142ff03f7441855a76f3b8c
Rebecca Mutoni <rbccmtni1gmail.com>
2025-05-13 17:47:40 +0200

    chore: add the crud

--- Controllers/PizzaController.cs
+++ Controllers/PizzaController.cs
@@ -4,6 +4,8 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace ContosoPizza.Controllers;
 
+[ApiController]
+[Route("[controller]")]
 public class PizzaController: ControllerBase {
     public PizzaController(){
 
@@ -20,9 +22,32 @@ public class PizzaController: ControllerBase {
         return pizza;
     }
     // Posst action
+    [HttpPost]
+    public IActionResult Create(Pizza pizza){
+        PizzaService.Add(pizza);
+        return CreatedAtAction(nameof(Get), new {id = pizza.Id}, pizza);
+    // CreatedAtAction creates a 201 response with the location of the new resource
+    }
+
 
     // Put action
+    [HttpPut("{id}")]
+    public IActionResult Update(int id, Pizza pizza){
+       if (id != pizza.Id) return BadRequest();
+       var existingPizza = PizzaService.Get(id);
+       if(existingPizza is null) return NotFound();
+       PizzaService.Update(pizza);
+       return NoContent();
+       // NoContent returns a 204 response with no content
+    }
 
     // Delete action
+    [HttpDelete("{id}")]
+    public IActionResult Delete(int id){
+        var pizza = PizzaService.Get(id);
+        if(pizza is null) return NotFound();
+        PizzaService.Delete(id);
+        return NoContent();
+    }
 
 }
